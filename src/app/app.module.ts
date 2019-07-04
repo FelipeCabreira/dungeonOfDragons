e5f85ca783afd0b55775170cfe10ea1d4899bc41
Felipe Cabreira <felipe.cabreira@acad.pucrs.br>
2019-07-04 00:33:44 -0300

    feat(add): cleaning default items from ng new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DungeonComponent } from './_modules/dungeon/dungeon.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DungeonComponent
   ],
   imports: [
     BrowserModule,
