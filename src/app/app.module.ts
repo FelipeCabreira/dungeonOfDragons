95f417ec199e561f814a5e588e130ebb8765978f
Felipe Cabreira <felipe.cabreira@acad.pucrs.br>
2019-07-04 00:57:43 -0300

    feat(add): creating first components that may actually be added ( depending on creativity )

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DungeonComponent } from './_modules/dungeon/dungeon.component';
+import { DragonsComponent } from './_modules/dragons/dragons.component';
+import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './_modules/header/header.component';
+import { FooterComponent } from './_modules/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DungeonComponent,
+    DragonsComponent,
+    LoginComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
