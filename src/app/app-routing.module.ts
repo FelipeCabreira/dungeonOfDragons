9692a8929983a6969f8b7a521c4347d459a3968b
Felipe Cabreira <cabreira92@gmail.com>
2019-07-04 23:57:56 -0300

    fix(package.lock): packages erros

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { DungeonComponent } from './_modules/dungeon/dungeon.component';
 
 
 const routes: Routes = [
   // {
   //   path: '',
   //   redirectTo: 'dungeon',
-  //   pathMatch: 'full'
+  //   pathMatch: 'full',
   // },
-  // {
-  //   path: 'login',
-  //   component: LoginComponent
-  // }
-  // { path: '**', redirectTo: 'dungeon', pathMatch: 'full' },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  // { path: '**', redirectTo: 'login', pathMatch: 'full' },
 ];
 
 @NgModule({
